a9e131429bd68213cc1faf2121da85f70388e333
Diego Rustnnes Bandeira <rustnnes@gmail.com>
2020-10-28 22:10:57 -0300

    sonar suggests + repository tests

--- src/test/java/com/rodrigofujioka/dev/web/repository/DisciplinaRepositoryTest.java
+++ src/test/java/com/rodrigofujioka/dev/web/repository/DisciplinaRepositoryTest.java
@@ -1,34 +1,31 @@
 package com.rodrigofujioka.dev.web.repository;
 
 import com.rodrigofujioka.dev.web.domain.Disciplina;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
-@SpringBootTest
+@SpringBootTest(
+				webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestEntityManager
+@Transactional
 public class DisciplinaRepositoryTest {
 
 	@Autowired
-	private TestEntityManager entityManager;
+	private EntityManager entityManager;
 
 	@Autowired
 	private DisciplinaRepository disciplinaRepository;
-
-	@BeforeEach
-	void setUp() {
-	}
-
-	@AfterEach
-	void tearDown() {
-	}
 
 	@Test
 	public void whenFindByProfessorJoao_thenReturnDisciplinaPortugues() {
@@ -42,7 +39,31 @@ public class DisciplinaRepositoryTest {
 						disciplinaRepository.findDisciplinaByProfessor("Joao").get(0);
 
 		// then
-		assertThat(found.getNome(), equals(portugues.getNome()));
+		assertThat(found.getNome(), equalTo(portugues.getNome()));
+	}
+
+	@Test
+	public void whenFindByNomeAndProfessor_thenReturnDisciplina() {
+		// given
+		Disciplina matematica1 = new Disciplina("geometria", "Suely");
+		Disciplina matematica2 = new Disciplina("trigonometria", "Andrea");
+		entityManager.persist(matematica1);
+		entityManager.persist(matematica2);
+		entityManager.flush();
+
+		// when
+		List<Disciplina> disciplinas =
+						disciplinaRepository
+										.findDisciplinaByNomeAndProfessor("geometria","Suely");
+
+		Disciplina found =
+						disciplinas.stream()
+										.filter(d -> d.getProfessor().equalsIgnoreCase("Suely"))
+										.collect(Collectors.toList())
+										.get(0);
+
+		// then
+		assertThat(found.getNome(), equalTo(matematica1.getNome()));
 	}
 
 }
